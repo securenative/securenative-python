45243623c8a8e43de6d75e984bd9001626f0d7cb
Inbal Tako <inbal@securenative.com>
2020-08-24 13:06:36 +0300

    SN-1938 Validate user id

--- tests/api_manager_test.py
+++ tests/api_manager_test.py
@@ -10,6 +10,7 @@ from securenative.enums.risk_level import RiskLevel
 from securenative.event_manager import EventManager
 from securenative.event_options_builder import EventOptionsBuilder
 from securenative.exceptions.securenative_invalid_options_exception import SecureNativeInvalidOptionsException
+from securenative.models.event_options import EventOptions
 from securenative.models.user_traits import UserTraits
 from securenative.models.verify_result import VerifyResult
 
@@ -24,13 +25,11 @@ class ApiManagerTest(unittest.TestCase):
                 "user-agent": "Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"
             }).build()
 
-        self.event_options = EventOptionsBuilder(EventTypes.LOG_IN). \
-            with_user_id("USER_ID"). \
-            with_user_traits(UserTraits("USER_NAME", "USER_EMAIL", "+12012673412")). \
-            with_context(self.context). \
-            with_properties({"prop1": "CUSTOM_PARAM_VALUE",
-                             "prop2": True,
-                             "prop3": 3}).build()
+        self.event_options = EventOptions(EventTypes.LOG_IN, "USER_ID",
+                                          UserTraits("USER_NAME", "USER_EMAIL", "+12012673412"), context=self.context,
+                                          properties={"prop1": "CUSTOM_PARAM_VALUE",
+                                                      "prop2": True,
+                                                      "prop3": 3})
 
     @responses.activate
     def test_track_event(self):
