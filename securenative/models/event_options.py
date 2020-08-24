ff2466fa22a7f5a87380026f6a7cfd68be143ff5
Inbal Tako <inbal@securenative.com>
2020-08-24 13:00:20 +0300

    SN-1938 Validate user id

--- securenative/models/event_options.py
+++ securenative/models/event_options.py
@@ -1,6 +1,6 @@
 class EventOptions(object):
 
-    def __init__(self, event, user_id=None, user_traits=None,
+    def __init__(self, event, user_id, user_traits=None,
                  context=None, properties=None, timestamp=None):
         self.event = event
         self.user_id = user_id
