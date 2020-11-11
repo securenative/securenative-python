ca9038e624b5b9f428e28f669edba97a8ed8c10e
Inbal Tako <inbal@securenative.com>
2020-11-11 11:56:28 +0200

    Add ip extraction tests

--- tests/request_utils_test.py
+++ tests/request_utils_test.py
@@ -40,3 +40,271 @@ class RequestUtilsTest(unittest.TestCase):
             client_ip = RequestUtils.get_client_ip_from_request(request, options)
 
             self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_HTTP_X_FORWARDED_FOR_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "HTTP_X_FORWARDED_FOR": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_HTTP_X_FORWARDED_FOR_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "HTTP_X_FORWARDED_FOR": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_X_FORWARDED_FOR_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "X_FORWARDED_FOR": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_X_FORWARDED_FOR_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "X_FORWARDED_FOR": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_REMOTE_ADDR_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "REMOTE_ADDR": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_REMOTE_ADDR_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "REMOTE_ADDR": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_forwarded_for_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-forwarded-for": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_forwarded_for_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-forwarded-for": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_client_ip_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-client-ip": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_client_ip_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-client-ip": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_real_ip_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-real-ip": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_real_ip_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-real-ip": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_forwarded_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-forwarded": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_forwarded_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-forwarded": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_cluster_client_ip_for_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-cluster-client-ip": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_x_cluster_client_ip_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-client-ip": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_forwarded_for_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "forwarded-for": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_forwarded_for_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "forwarded-for": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_forwarded_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "forwarded": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_forwarded_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "forwarded": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_via_for_header_single_ip(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "via": "141.246.115.116"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_from_via_header_multiple_ips(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "via": "141.246.115.116, 203.0.113.1, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("141.246.115.116", client_ip)
+
+    def test_extraction_priority_with_x_forwarded_for(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-forwarded-for": "203.0.113.1",
+                "x-real-ip": "198.51.100.101",
+                "x-client-ip": "198.51.100.102"
+            }
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("203.0.113.1", client_ip)
+
+    def test_extraction_priority_without_x_forwarded_for(self):
+        options = SecureNativeOptions()
+
+        with requests_mock.Mocker(real_http=True) as request:
+            request.headers = {
+                "x-real-ip": "198.51.100.101",
+                "x-client-ip": "203.0.113.1, 141.246.115.116, 12.34.56.3"}
+
+        client_ip = RequestUtils.get_client_ip_from_request(request, options)
+
+        self.assertEqual("203.0.113.1", client_ip)
